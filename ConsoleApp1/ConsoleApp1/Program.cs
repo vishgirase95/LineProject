93f751e1081019b8451e49695e85cec58a42ba32
vish <vishgirase95@gmail.com>
2021-10-21 15:51:13 +0530

    [vishal] added line length cal

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,18 @@ namespace ConsoleApp1
 {
     class Program
     {
+      
+        public static double lineLength(int x1,int y1, int x2,int y2)
+        {
+
+            double LengthOfLine = Math.Sqrt((x2 - x1) + (y2 - y1));
+            Console.WriteLine(LengthOfLine);
+            return LengthOfLine;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Well Come to Line Comparision Computation program on master branch");
+            lineLength(10, 5, 15, 5);     
         }
     }
 }
