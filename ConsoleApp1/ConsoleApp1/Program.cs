832fe4eecec0fe7ec8d46686a7d6a459e53ae0d3
vish <vishgirase95@gmail.com>
2021-10-29 12:00:24 +0530

    [Vishal] added use CommpareTo

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,27 +5,28 @@ namespace ConsoleApp1
     class Program
     {
       
-        public static double lineLength(double x1,double y1, double x2,double y2)
+        public static double lineLength(double x1,double y1, double x2,double y2, double a)
         {
 
             double LengthOfLine = Math.Sqrt((x2 - x1) + (y2 - y1));
-            Console.WriteLine(LengthOfLine + "length of line ");
+            LengthOfLine = Math.Round(LengthOfLine, 2);
+            Console.WriteLine(LengthOfLine + "   length of line " +a);
             return LengthOfLine;
         }
         public static void compare(double line1, double line2)
         {
-            if (line1 == line2)
+            if (line1.CompareTo(line2)==0)
             {
                 Console.WriteLine("Both Lines are equal");
             }
-            else if(line1 > line2)
+            else if(line1.CompareTo(line2)>0)
             {
-                Console.WriteLine("line1greater than line2");
+                Console.WriteLine("line_1 greater than line_2");
 
             }
-            else if (line1 < line2)
+            else if (line1.CompareTo(line2)<0)
             {
-                Console.WriteLine("line1 lss than line2");
+                Console.WriteLine("line_1 is  less than line_2");
             }
             else
             {
@@ -42,8 +43,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Well Come to Line Comparision Computation program on master branch");
-            double line1 = lineLength(10, 5, 15, 10);
-            double line2 = lineLength(20, 10, 30, 20);
+            double line1 = lineLength(10, 5, 15, 10, 1);
+            double line2 = lineLength(20, 10, 30, 20,2);
             compare(line1,line2);
 
         }
